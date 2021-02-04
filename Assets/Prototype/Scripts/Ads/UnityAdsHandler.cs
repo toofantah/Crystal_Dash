4ef0f7a82ef916142737a0b571dbe0dd73ce3b3b
toofantah <toofan.tah@gmail.com>
2021-02-04 20:55:56 +0300

    Update: Unity Ads Package

---
+++ Assets/Prototype/Scripts/Ads/UnityAdsHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class UnityAdsHandler : MonoBehaviour
+{
+
+    [SerializeField] string googlePlay_ID="4001611";
+    bool testMode = true;
+
+    void Start()
+    {
+        Advertisement.Initialize(googlePlay_ID, testMode);
+    }
+     
+
+    public void DisplayInteratialAds()
+    {
+        Advertisement.Show();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
